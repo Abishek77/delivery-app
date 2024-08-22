5c3a3baca134aadf69b7f3d3b6e2ca48d3e786e6
Ganesh-Mern <raghabm7@gmail.com>
2024-08-22 19:06:42 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -14,21 +14,20 @@ const Login = () => {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const phonePattern = /^[0-9]{7,15}$/;
 
-    if (username === '' || password === '') {
-      toast.error('All fields are required', {
+    if (username === "" || password === "") {
+      toast.error("All fields are required", {
         autoClose: 1000,
         hideProgressBar: true,
       });
       return;
     } else if (!emailPattern.test(username) && !phonePattern.test(username)) {
-      toast.error('Please enter a valid email or phone number', {
+      toast.error("Please enter a valid email or phone number", {
         autoClose: 1000,
         hideProgressBar: true,
       });
       return;
-    } 
-    else if (password.length < 2 || password.length > 4) {
-      toast.error('Password must be between 2 and 4 characters long', {
+    } else if (password.length < 8 || password.length > 12) {
+      toast.error("Password must be between 8 and 12 characters long", {
         autoClose: 1000,
         hideProgressBar: true,
       });
@@ -40,42 +39,39 @@ const Login = () => {
       };
 
       axios
-        .get('https://minitgo.com/api/delivery_boy_reg.php')
+        .post("https://minitgo.com/api/delivery_boy_reg.php", data)
         .then((response) => {
-          console.log('response', response.data.data);
-         
-        
-          if (response.data && response.data.data.length > 0) {
+          console.log("response", response.data);
+
+          if (response.data.status === true) {
+            console.log("hiii");
+            console.log(response.data.data);
+
             const allUsers = response.data.data;
-           
-            // Check if the userid is an email or a phone number
-            const foundUser = allUsers.find(
-              (user) => user.email === data.userid || user.phone_number === data.userid
-            );
- 
-            if (foundUser) {
-              if (foundUser.password === data.password) {
-                
+            console.log("allusers", allUsers.password);
+
+            if (allUsers) {
+              if (allUsers.password === data.password) {
                 console.log("Login successful");
-               
+
                 toast.success("Login successful", {
                   autoClose: 1000,
                   hideProgressBar: true,
                 });
-                navigate('/dashboard');
- 
+                navigate("/dashboard");
+
                 const userData = {
-                  userId: foundUser.dbid,
-                  fullName: foundUser.name,
-                  phoneNumber: foundUser.phone,
-                  email: foundUser.email,
-                  address: foundUser.address
+                  userId: allUsers.dbid,
+                  fullName: allUsers.name,
+                  phoneNumber: allUsers.phone,
+                  email: allUsers.email,
+                  address: allUsers.address,
                 };
- 
+
                 localStorage.setItem("user", JSON.stringify(userData));
- 
+
                 console.log("FOUND USER:", userData);
- 
+
                 setUsername("");
                 setPassword("");
               } else {
@@ -100,8 +96,8 @@ const Login = () => {
           }
         })
         .catch((error) => {
-          console.error('Failed to fetch user information:', error);
-          toast.error('Login failed. Please try again.', {
+          console.error("Failed to fetch user information:", error);
+          toast.error("Login failed. Please try again.", {
             autoClose: 1000,
             hideProgressBar: true,
           });
@@ -114,10 +110,13 @@ const Login = () => {
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <div className="card my-5">
-            <form className="card-body   rounded  p-lg-5" onSubmit={handleSubmit}>
+            <form
+              className="card-body   rounded  p-lg-5"
+              onSubmit={handleSubmit}
+            >
               <div className="text-center mb-2 pb-2">
-                <h2 className='text-secondary fs-1'>Login</h2>
-                <p className=' fs-6 text-success'>Delivery partner's</p>
+                <h2 className="text-secondary fs-1">Login</h2>
+                <p className=" fs-6 text-success">Delivery partner's</p>
               </div>
               <div className="mb-3">
                 <label className="mb-2">Email/Phone</label>
@@ -138,7 +137,8 @@ const Login = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-              </div><br></br>
+              </div>
+              <br></br>
               <div className="text-center">
                 <button type="submit" className="btn btn-secondary px-5 mb-3">
                   Login
